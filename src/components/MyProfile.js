c5d24f103c6a7e15247eb54bdb27b3c5b2c7ef1c
culiutudousi <chenzhicheng1994@gmail.com>
2020-09-23 15:42:30 +0800

    add logics without style

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -2,9 +2,75 @@ import React, {Component} from 'react';
 import './myProfile.less';
 
 class MyProfile extends Component {
+  state = {
+    name: '',
+    gender: 'male',
+    description: '',
+    agreement: false
+  }
+
+  handleChange = (event) => {
+    if (event.target.name === "agreement") {
+      this.setState({
+        "agreement": !this.state.agreement
+      });
+      return;
+    }
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
     return (
-      <form></form>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <br/>
+          <input 
+            type="text"
+            name="name"
+            onChange={this.handleChange}
+            value={this.state.name}
+            placeholder="Your name"
+          />
+        </label>
+        <br/>
+        <label>
+          Gender
+          <br/>
+          <select name="gender" value={this.state.gender} onChange={this.handleChange}>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </label>
+        <br/>
+        <label>
+          Description
+          <br/>
+          <textarea 
+            name="description" 
+            value={this.state.description} 
+            placeholder="Description about yourself"
+            onChange={this.handleChange}
+          />
+        </label>
+        <br/>
+        <label>
+          <input type="checkbox" name="agreement" onChange={this.handleChange}/>
+          <span>I have read the terms of conduct</span>
+        </label>
+        <br/>
+        <button 
+          type="submit" 
+          disabled={!this.state.name || !this.state.description || !this.state.agreement}
+        >Submit</button>
+      </form>
     );
   }
 }
